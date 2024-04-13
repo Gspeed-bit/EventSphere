5a0f8b591b6b2e7d992f86580d16c6ca7f143b4f
Ayodeji Daniel Adeyeye <adeyeye17@gmail.com>
2024-04-13 14:46:58 +0200

    run build

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,9 +2,9 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     // this layout was created just to be able to style the login and logout page
     <div className=" flex-center min-h-screen bg-primary-50 bg-cover bg-fixed bg-dotted-pattern w-full bg-center ">
-      {children}
+      {children} 
     </div>
   );
 };
-
+//
 export default Layout;
