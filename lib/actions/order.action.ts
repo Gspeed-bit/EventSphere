05f845f8245caef18b1bb1febd1927ebb0c3aed9
Adeyeye Ayodeji Daniel <adeyeye17@gmail.com>
2024-04-28 23:54:39 +0200

    stripe done
    payment session done

--- lib/actions/order.action.ts
+++ lib/actions/order.action.ts
@@ -1,11 +1,13 @@
 "use server";
 
-import { CheckoutOrderParams } from "@/types";
+import { CheckoutOrderParams, CreateOrderParams } from "@/types";
 
 import { handleError } from "../utils";
 
 import { Stripe } from "stripe";
 import { redirect } from "next/navigation";
+import { connectToDatabase } from "../database";
+import Order from "../database/models/order.model";
 
 export const checkoutOrder = async (order: CheckoutOrderParams) => {
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
@@ -40,3 +42,20 @@ export const checkoutOrder = async (order: CheckoutOrderParams) => {
     throw error;
   }
 };
+
+export const createOrder = async (order: CreateOrderParams) => {
+  try {
+    await connectToDatabase();
+
+    const newOrder = await Order.create({
+      ...order,
+      event: order.eventId,
+      buyer: order.buyerId,
+    });
+
+    return JSON.parse(JSON.stringify(newOrder));
+  } catch (error) {
+    handleError(error);
+  }
+};
+
